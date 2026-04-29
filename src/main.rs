d0e0fda0aee82c249e565bdea7ff9720353db74c
ashiven <nevisha@pm.me>
2026-04-29 12:28:42 +0200

    fix tests

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use mimir_eqsat::{
 use std::fs;
 
 fn main() {
-    let example = fs::read_to_string("./examples/pow.slotted").expect("Failed to read file.");
+    let example = fs::read_to_string("./examples/import.slotted").expect("Failed to read file.");
     let nodes = equality_saturate_slotted(&example, vec![RuleSet::Default], CostFn::AstSize);
 
     print!("{}", pretty_ffi(nodes, 80));
