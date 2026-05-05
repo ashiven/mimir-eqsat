22129b1d83ca17cd694305de6b4b86216fcf2f35
ashiven <nevisha@pm.me>
2026-05-05 17:19:26 +0200

    inline header functions and cargo rebuild on build

--- include/mim/plug/eqsat/eqsat.h
+++ include/mim/plug/eqsat/eqsat.h
@@ -8,7 +8,7 @@ namespace mim {
 
 // fun extern _impl(): %eqsat.Impl =
 //     return <impl>;
-void eqsat_impl(World& world, flags_t impl) {
+inline void eqsat_impl(World& world, flags_t impl) {
     auto Impl     = world.annex<plug::eqsat::Impl>();
     auto _impl    = world.mut_fun({}, Impl)->set("_impl");
     auto ret      = _impl->var(2, 1);
@@ -19,7 +19,7 @@ void eqsat_impl(World& world, flags_t impl) {
 
 // fun extern _cost_fun(): %eqsat.CostFun =
 //     return <cost_fun>;
-void eqsat_cost_fun(World& world, flags_t cost_fun) {
+inline void eqsat_cost_fun(World& world, flags_t cost_fun) {
     auto CostFun      = world.annex<plug::eqsat::CostFun>();
     auto _cost_fun    = world.mut_fun({}, CostFun)->set("_cost_fun");
     auto ret          = _cost_fun->var(2, 1);
@@ -30,7 +30,7 @@ void eqsat_cost_fun(World& world, flags_t cost_fun) {
 
 // fun extern _rulesets(): %eqsat.Ruleset =
 //     return %eqsat.rulesets (<rulesets>);
-void eqsat_rulesets(World& world, std::vector<flags_t> rulesets) {
+inline void eqsat_rulesets(World& world, std::vector<flags_t> rulesets) {
     auto Ruleset   = world.annex<plug::eqsat::Ruleset>();
     auto _rulesets = world.mut_fun({}, Ruleset)->set("_rulesets");
     auto ret       = _rulesets->var(2, 1);
@@ -50,7 +50,7 @@ void eqsat_rulesets(World& world, std::vector<flags_t> rulesets) {
 
 // fun extern _rules(): %eqsat.Rules =
 //     return %eqsat.rules (<rules>);
-void eqsat_rules(World& world, DefVec rules) {
+inline void eqsat_rules(World& world, DefVec rules) {
     auto Rules  = world.annex<plug::eqsat::Rules>();
     auto _rules = world.mut_fun({}, Rules)->set("_rules");
     auto ret    = _rules->var(2, 1);
