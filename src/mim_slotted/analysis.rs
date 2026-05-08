ba5a0e5ac5e1c7ea282cfca5c18b6914fd14ba9e
ashiven <nevisha@pm.me>
2026-05-08 16:29:54 +0200

    implement analysis merge

--- src/mim_slotted/analysis.rs
+++ src/mim_slotted/analysis.rs
@@ -9,6 +9,16 @@ pub struct AnalysisData {
     pub type_: Option<TypeExpr>,
 }
 
+// We compare type expressions by their ast sizes so we
+// can do a quick comparison of equivalent types in the
+// analysis and pick the one with a smaller ast size
+fn term_size(type_expr: &TypeExpr) -> usize {
+    fn size(type_expr: &TypeExpr) -> usize {
+        1 + type_expr.children.iter().map(size).sum::<usize>()
+    }
+    size(type_expr)
+}
+
 // TODO: Merge and make for types
 impl Analysis<MimSlotted> for MimSlottedAnalysis {
     type Data = AnalysisData;
@@ -17,8 +27,32 @@ impl Analysis<MimSlotted> for MimSlottedAnalysis {
         AnalysisData::default()
     }
 
-    fn merge(_l: Self::Data, _r: Self::Data) -> Self::Data {
-        AnalysisData::default()
+    // We are making the assumption here that terms are already
+    // well-typed. (terms emitted from the mim compiler are already well-typed
+    // and we do our best to correctly type terms that are newly introduced by
+    // rewrite-rules) So whenever we are merging two eclasses associated with type-data,
+    // we assume they are equivalent representations of the same type and just
+    // merge the type with the smaller term-size into the eclass.
+    fn merge(l: Self::Data, r: Self::Data) -> Self::Data {
+        match (l.type_, r.type_) {
+            (None, None) => AnalysisData { type_: None },
+            (None, Some(type_r)) => AnalysisData {
+                type_: Some(type_r),
+            },
+            (Some(type_l), None) => AnalysisData {
+                type_: Some(type_l),
+            },
+            (Some(type_l), Some(type_r)) => {
+                let merged_type = if term_size(&type_l) < term_size(&type_r) {
+                    type_l
+                } else {
+                    type_r
+                };
+                AnalysisData {
+                    type_: Some(merged_type),
+                }
+            }
+        }
     }
 
     fn modify(_eg: &mut EGraph<MimSlotted, Self>, _id: Id) {}
