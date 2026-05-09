de0d36e5b125801d93d3079b5096fbf97631383d
ashiven <nevisha@pm.me>
2026-05-09 13:17:55 +0200

    update rise ruleset

--- src/mim_slotted/rulesets/rise.rs
+++ src/mim_slotted/rulesets/rise.rs
@@ -26,13 +26,13 @@ pub fn rules() -> Vec<RW> {
 }
 
 fn beta() -> RW {
-    let pat = "(app (con ?dom-type $dom (scope ?filter ?body)) ?e)";
-    let outpat = "(let var$ (scope ?e ?body))";
+    let pat = "(app (lam $x (scope ?filter ?body)) ?e)";
+    let outpat = "(let x$ (scope ?e ?body))";
     Rewrite::new("beta", pat, outpat)
 }
 
 fn eta() -> RW {
-    let pat = "(con ?dom-type $dom (scope ?filter (app ?fn (var $dom))))";
+    let pat = "(lam $x (scope ?filter (app ?fn (var $x))))";
     let outpat = "?fn";
 
     // On condition that $dom is not bound in ?fn because this makes
@@ -45,9 +45,7 @@ fn eta() -> RW {
 
 fn eta_expansion() -> RW {
     let pat = "?fn";
-    // TODO: How do we infer the dom-type of the con?
-    // - an analysis that stores types on eclasses and finds the type of ?fn ?
-    let outpat = "(con DOM-TYPE $dom (scope (lit ff Bool) (app ?fn (var $dom))))";
+    let outpat = "(lam $x (scope (lit ff Bool) (app ?fn (var $x))))";
     Rewrite::new("eta-expansion", pat, outpat)
 }
 
@@ -87,16 +85,16 @@ fn let_app_unopt() -> RW {
 }
 
 fn let_lam_diff() -> RW {
-    let pat = "(let $name (scope ?def (con ?dom-type $dom (scope ?filter ?body))))";
-    let outpat = "(con ?dom-type $dom (scope ?filter (let $name (scope ?def ?body))))";
+    let pat = "(let $name (scope ?def (lam $x (scope ?filter ?body))))";
+    let outpat = "(lam $x (scope ?filter (let $name (scope ?def ?body))))";
     Rewrite::new_if("let-lam-diff", pat, outpat, |subst, _| {
         subst["body"].slots().contains(&Slot::named("name"))
     })
 }
 
 fn let_lam_diff_unopt() -> RW {
-    let pat = "(let $name (scope ?def (con ?dom-type $dom (scope ?filter ?body))))";
-    let outpat = "(con ?dom-type $dom (scope ?filter (let $name (scope ?def ?body))))";
+    let pat = "(let $name (scope ?def (lam $x (scope ?filter ?body))))";
+    let outpat = "(lam $x (scope ?filter (let $name (scope ?def ?body))))";
     Rewrite::new("let-lam-diff", pat, outpat)
 }
 
@@ -106,23 +104,22 @@ fn let_lam_diff_unopt() -> RW {
 // ((map f) ((map g) arg)) => ((map λx.(f (g x))) arg)
 fn map_fusion() -> RW {
     let pat = "(app (app %rise.map ?f) (app (app %rise.map ?g) ?arg))";
-    let outpat =
-        "(app (app %rise.map (con ?x-type $x (scope ?filter (app ?f (app ?g (var $x)))))) ?arg)";
+    let outpat = "(app (app %rise.map (lam $x (scope ?filter (app ?f (app ?g (var $x)))))) ?arg)";
     Rewrite::new("map-fusion", pat, outpat)
 }
 
 // (map λx.(f (g x))) => λy.((map f) ((map λx.(g x)) y))
 fn map_fission() -> RW {
-    let pat = "(app %rise.map (con ?x-type $x (scope ?filter (app ?f ?gx))))";
+    let pat = "(app %rise.map (lam $x (scope ?filter (app ?f ?gx))))";
     // TODO: What do we write for y-type?
     let outpat = "
-(con y-type $y 
+(lam $y 
     (scope
         (lit ff Bool)
         (app
             (app %rise.map ?f)
             (app
-                (app %rise.map (con ?x-type $x (scope ?filter ?gx)))
+                (app %rise.map (lam $x (scope ?filter ?gx)))
                 (var $y)))))";
     Rewrite::new_if("map-fission", pat, outpat, |subst, _| {
         !subst["f"].slots().contains(&Slot::named("x"))
